6261abc91f99ec2f9d38f5d57f1c14fdbe1a341f
Henry SU <hsu2@slb.com>
2020-04-20 11:09:39 +0800

    initial commit

---
+++ shuffle/models/GameSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace shuffle.models
+{
+    public class GameSetting
+    {
+        [JsonProperty("Attendees")] public List<Attendee> Attendees { get; set; }
+        [JsonProperty("Rule")] public GameRule Rule { get; set; }
+    }
+}
---
+++ shuffle/models/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace shuffle.models
+{
+    public class Player
+    {
+        public Player(Attendee attendee, Role role)
+        {
+            Attendee = attendee;
+            Role = role;
+        }
+        public Attendee Attendee { get; set; }
+        public Role Role { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Attendee}, role: {Role}";
+        }
+    }
+}
